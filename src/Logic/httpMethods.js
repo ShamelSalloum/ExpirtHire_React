aad5d5110d9d9e4052cc0e8d804d4bc073f8517a
ShamelSalloum <101184232+ShamelSalloum@users.noreply.github.com>
2023-08-16 17:43:55 +0300

    Update httpMethods.js

--- src/Logic/httpMethods.js
+++ src/Logic/httpMethods.js
@@ -1,5 +1,5 @@
 export const BaseUrl = "https://expert-hire-api.onrender.com/api/v1";
-export const AdminBaseUrl = "http://localhost:4000/api/v1/admin";
+export const AdminBaseUrl = "https://expert-hire-api.onrender.com/api/v1/admin";
 
 export function extractDate({ date }) {
   const [datePart] = date.split("T");
